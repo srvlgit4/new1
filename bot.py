142b98caed917e6a519a1c473a9dcc8c2c6d38a6
srvlgit4 <srvl78052+git4@gmail.com>
2026-05-03 13:14:54 +0530

    Change bot token in bot.py
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from telegram.error import RetryAfter
 # ==========================================
 # CONFIGURATION
 # ==========================================
-TOKEN = "7848041378:AAGewjtCvIDobmDLE6wFFUI1ETduxSkQtsE" # Your Bot Token
+TOKEN = "7848041378:AAH_r3VM726jFyPuzZ-G1WDucIrAL6Wo6o8" # Your Bot Token
 DEFAULT_DOCX_CHUNK = 50
 DEFAULT_EPUB_CHUNK = 500
 
